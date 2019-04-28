ac5b2876d11c794ecef8161e6822a917ee78d487
Myron Rotter <myronrotter01@t-online.de>
2019-04-28 13:21:50 +0200

    Implement tracking in Utility, adjust IUtility accordingly
    Change uint256 to int256. Add unctions increaseRenewableEnergy,
    increaseNonRenewableEnergy, decreaseRenewableEnergy,
    decreaseNonRenewableEnergy and getHousehold.
    Add function descriptions in IUtility.

--- contracts/IUtility.sol
+++ contracts/IUtility.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.5.0 <0.6.0;
  */
 interface IUtility {
 
-  // event for energy transfer triggered by netting
+  // event for energy transfer triggered by settlement (netting)
   event EnergyTransfer(address indexed household, uint updatedBalance);
 
   /**
@@ -16,13 +16,50 @@ interface IUtility {
    */
   function addHousehold(address _household) external returns (bool);
 
-  function increaseEnergy(address producer, uint addedValue) external returns (bool);
+  /**
+   * @dev Increase the amount of renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function increaseRenewableEnergy(address _household, int256 _value) external returns (bool);
 
-  function decreaseEnergy(address consumer, uint subtractedValue) external returns (bool);
+  /**
+   * @dev Increase the amount of non-renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function increaseNonRenewableEnergy(address _household, int256 _value) external returns (bool);
+
+  /**
+   * @dev Decrease the amount of renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function decreaseRenewableEnergy(address _household, int256 _value) external returns (bool);
+
+  /**
+   * @dev Decrease the amount of non-renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function decreaseNonRenewableEnergy(address _household, int256 _value) external returns (bool);
 
   function settle() external returns (bool);
 
-  function totalEnergy() external view returns (uint);
+  function totalEnergy() external view returns (int256);
 
-  function balanceOf(address household) external view returns (uint);
+  function totalRenewableEnergy() external view returns (int256);
+
+  function totalNonRenewableEnergy() external view returns (int256);
+
+  /**
+   * @dev Get energy properties of _household
+   * @param _household address of the household owner/ parity node ?
+   * @return properties (initialized, energy, renewableEnergy, nonRenewableEnergy) of _household if _household exists
+   */
+  function getHousehold(address _household) external view returns (bool, int256, int256, int256);
 }
--- contracts/Utility.sol
+++ contracts/Utility.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./IUtility.sol";
+import "./Mortal.sol";
 
 
 /**
@@ -8,47 +9,42 @@ import "./IUtility.sol";
  * @notice Tracks production/consumption of energy of all households. Settles energy requests by distributing existing energy as fair as possible (netting).
  * @dev Implements interface IUtility.
  */
-contract Utility is IUtility {
-  address owner;
+contract Utility is IUtility, Mortal {
+  /*
+   * electrical work/energy W, unit 1 kWh = 1000 Wh = 1000 W * 3600 s = 3,6 * 10^6 Ws
+   * (int256) 1 means 1 Ws?
+   */
+
   // total energy in the system
-  uint256 totalEnergy;
+  int256 public totalEnergy;
   // total renewable energy in the system
-  uint256 totalRenewableEnergy;
+  int256 public totalRenewableEnergy;
   // total non-renewable energy in the system
-  uint256 totalNonRenewableEnergy;
+  int256 public totalNonRenewableEnergy;
 
   struct Household {
     // for checks if household exists
     bool initialized;
 
     // total energy household
-    uint256 energy;
+    int256 energy;
 
     // total renewable/ non-renewable energy household
-    uint256 renewableEnergy;
-    uint256 nonRenewableEnergy;
+    int256 renewableEnergy;
+    int256 nonRenewableEnergy;
   }
 
   // mapping of all households
   mapping(address => Household) households;
 
-  modifier onlyOwner(address _owner) {
-    require(msg.sender == owner, "Only owner may access this function!");
-    _;
-  }
-
-  constructor (address _owner) public {
-    owner = _owner;
-  }
-
   // modifier such that only some authority can access functions
   modifier onlyEnergyAuthority() {
-    // require(msg.sender == );
+    // require(msg.sender == , "Only some authority may access this function.");
     _;
   }
 
   /**
-   * @dev Create a household to track energy production and consumption
+   * @dev Create a household with address _household to track energy production and consumption
    * @param _household address of the household owner/ parity node ?
    * @return success bool if household does not already exists, should only be called by some authority
    */
@@ -62,5 +58,93 @@ contract Utility is IUtility {
     hh.renewableEnergy = 0;
     hh.nonRenewableEnergy = 0;
     return true;
+  }
+
+  /**
+   * @dev Increase the amount of renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function increaseRenewableEnergy(address _household, int256 _value) external returns (bool) {
+    require(households[_household].initialized == true, "Household does not exist.");
+    require(_value > 0, "_value is zero or negative");
+
+    households[_household].renewableEnergy = households[_household].renewableEnergy + _value;
+    households[_household].energy = households[_household].renewableEnergy + households[_household].nonRenewableEnergy;
+
+    totalRenewableEnergy = totalRenewableEnergy + _value;
+    totalEnergy = totalRenewableEnergy + totalNonRenewableEnergy;
+    return true;
+  }
+
+  /**
+   * @dev Increase the amount of non-renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function increaseNonRenewableEnergy(address _household, int256 _value) external returns (bool) {
+    require(households[_household].initialized == true, "Household does not exist.");
+    require(_value > 0, "_value is zero or negative");
+
+    households[_household].nonRenewableEnergy = households[_household].nonRenewableEnergy + _value;
+    households[_household].energy = households[_household].renewableEnergy + households[_household].nonRenewableEnergy;
+
+    totalNonRenewableEnergy = totalNonRenewableEnergy + _value;
+    totalEnergy = totalRenewableEnergy + totalNonRenewableEnergy;
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function decreaseRenewableEnergy(address _household, int256 _value) external returns (bool) {
+    require(households[_household].initialized == true, "Household does not exist.");
+    require(_value > 0, "_value is zero or negative");
+
+    households[_household].renewableEnergy = households[_household].renewableEnergy - _value;
+    households[_household].energy = households[_household].renewableEnergy + households[_household].nonRenewableEnergy;
+
+    totalRenewableEnergy = totalRenewableEnergy - _value;
+    totalEnergy = totalRenewableEnergy + totalNonRenewableEnergy;
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of non-renewable energy of _household by _value
+   * @param _household address of the household owner/ parity node ?
+   * @param _value int256 amount of energy
+   * @return success bool if household exists and _value > 0
+   */
+  function decreaseNonRenewableEnergy(address _household, int256 _value) external returns (bool) {
+    require(households[_household].initialized == true, "Household does not exist.");
+    require(_value > 0, "_value is zero or negative");
+
+    households[_household].nonRenewableEnergy = households[_household].nonRenewableEnergy - _value;
+    households[_household].energy = households[_household].renewableEnergy + households[_household].nonRenewableEnergy;
+
+    totalNonRenewableEnergy = totalNonRenewableEnergy - _value;
+    totalEnergy = totalRenewableEnergy + totalNonRenewableEnergy;
+    return true;
+  }
+
+  /**
+   * @dev Get energy properties of _household
+   * @param _household address of the household owner/ parity node ?
+   * @return properties (initialized, energy, renewableEnergy, nonRenewableEnergy) of _household if _household exists
+   */
+  function getHousehold(address _household) external view returns (bool, int256, int256, int256) {
+    require(households[_household].initialized == true, "Household does not exist.");
+
+    return (
+      households[_household].initialized,
+      households[_household].energy,
+      households[_household].renewableEnergy,
+      households[_household].nonRenewableEnergy
+    );
   }
 }
