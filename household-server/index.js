1479f9c224d239c2977f4e1ab4aad10896483f5f
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2019-04-29 16:14:54 +0200

    Opened Server and Added Eventlisteners
    index.js now opens a HTTPServer listening for requests, then emitting a event.
    db-handler & transcation-handler will then perform stuff

--- household-server/index.js
+++ household-server/index.js
@@ -1,5 +1,33 @@
-// TODO
-const MockData = require('./mock-sensor-data.js');
+const http = require('http');
+const events = require('events');
+const dbhandler = require('./db-handler')
+const txhandler  = require('./transaction-handler')
+const MockData = require('./mock-sensor-data');
 
-console.log('household server');
-MockData().createMockData(3,6,9);
+//Config of server
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Adding Event Listener
+var em = new events.EventEmitter(); 
+em.on('input', dbhandler)
+em.on('input', txhandler)
+
+
+const server = http.createServer((req, res) => {
+    console.log('Request received')
+
+    // Emitting event
+    //em.emit('input', req, res) //how it should work with a real smart meter
+    em.emit('input', MockData.createMockData(3,0,10), res ) // Currently Mocked within the household server
+   
+    // Sending Response
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Success\n');
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Household Server running at http://${hostname}:${port}/`);
+});
--- household-server/mock-sensor-data.js
+++ household-server/mock-sensor-data.js
@@ -1,14 +1,21 @@
-module.exports = function() {
+module.exports = {
 
-    return {
+    /**
+     * This function draws random samples from an uniform distribution.
+     * @param {uint} samples amount of samples
+     * @param {int} min min value of samples
+     * @param {int} max max value of samples (included)
+     * @return {list} List of samples with the length of samples 
+     */
         createMockData: function (samples, min, max) {
             let mockData = [];
             for (i = 0; i < samples; i++) {
-                let rndm = Math.random() * (max - min) + min;
+                let rndm = Math.random() * (max - min) + min 
+                //rounding the samples
+                rndm = Math.round(rndm * 100) / 100 
                 mockData.push(rndm);
             }
-            console.log(mockData);
-            return this;
-        }
+            return mockData;
+        
     }
 }
