4a177420aa160c2a49f9ccbb47244cb7454c5dde
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2019-05-01 10:25:54 +0200

    Added switch for incoming requests
    The incoming request is now filtered in a switch case block and then the respecting events are emitted.
    Also the events are defined in the EVENTS object to mitigate spelling mistakes

--- household-server/index.js
+++ household-server/index.js
@@ -8,26 +8,56 @@ const MockData = require('./mock-sensor-data');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Defining Events
+const EVENTS = {
+    SENSOR_INPUT: 'sensor_input',
+    UI_REQUEST: 'ui_request'
+}
 // Adding Event Listener
 var em = new events.EventEmitter(); 
-em.on('input', dbhandler)
-em.on('input', txhandler)
+em.on(EVENTS.SENSOR_INPUT, dbhandler)
+em.on(EVENTS.SENSOR_INPUT, txhandler)
 
-
+/**
+ * Creating the http server waiting for incoming requests
+ * When a request comes in, a corresponding event is emitted
+ * At last a response is sended to the requester
+ */
 const server = http.createServer((req, res) => {
-    console.log('Request received')
+    console.log(req.method, 'Request received')
+    var statusmsg= '';
 
-    // Emitting event
-    //em.emit('input', req, res) //how it should work with a real smart meter
-    em.emit('input', MockData.createMockData(3,0,10), res ) // Currently Mocked within the household server
+    switch(req.method){
+        // Get requests from the UI
+        case 'GET':
+            em.emit(EVENTS.UI_REQUEST, req, res );  // Currently Mocked within the household server
+            res.statusCode = 200;
+            statusmsg = 'Success'; break;
+
+        // PUT Requests from the Sensors
+        case 'PUT': 
+            em.emit(EVENTS.SENSOR_INPUT, MockData.createMockData(3,0,10), res);
+            res.statusCode = 200;
+            statusmsg = 'Success';  break;
+
+        // Default for any other
+        default: 
+            res.statusCode = 400;
+            statusmsg = req.method + ' is not supported. Try GET for UI Requests or PUT for Sensor data\n'; break;
+
+        }
+    
    
     // Sending Response
-    res.statusCode = 200;
+    console.log('Sending response')
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Success\n');
+    res.end(statusmsg);
 
 });
 
+/**
+ * Let the server listen to incoming requests on the given IP:Port
+ */
 server.listen(port, hostname, () => {
     console.log(`Household Server running at http://${hostname}:${port}/`);
 });
