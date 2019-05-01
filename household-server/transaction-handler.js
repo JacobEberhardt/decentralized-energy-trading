164f961bb8e74717eb3a8e651eb4cda45bb67e69
juliaholz <j.holz@campus.tu-berlin.de>
2019-05-01 15:04:53 +0200

    Creating and sending transactions using web3 implemented
    Adresses still missing.

--- household-server/transaction-handler.js
+++ household-server/transaction-handler.js
@@ -1,3 +1,23 @@
+// This Handler processes the input from the smart meters
 module.exports = (req, res) => {
-  console.log("Transaction Handler received: ", req);
+    console.log("Transaction Handler received: ", req);
+    const Web3 = require('web3');
+    const RpcURL = "";
+    const ContractAdress = "";
+    const ABI = "";
+    const HouseholdAdress= "";
+
+    //instantiate contract and web3
+    const web3 = new Web3(RpcURL);
+    var contract = new web3.eth.Contract(ABI, ContractAddress);
+
+    //prepare transactions
+    var produced = req.produce;
+    var consumed = req.consume;
+
+    //sign transactions
+    //to be done as we don't have a key yet
+
+    //send transaction
+    contract.methods.updateEnergy(123, produced, consumed).send();
 };
