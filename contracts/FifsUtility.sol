37bb470f4dfc285a74fdffcabc8c97712984de15
Myron Rotter <myronrotter01@t-online.de>
2019-05-03 19:51:49 +0200

    Continue on netting strategy (WIP)
    Currently, settle function in `FifsUtility.sol` divides `householdList`
    in households with renewable energy for settlement
    `householdListWithEnergy` and households with no renewable energy for
    settlement `householdListNoEnergy`. Further, sums up the total amount of
    available renewable energy for settlement.
    Discussion of seperating this task in private function.

--- contracts/FifsUtility.sol
+++ contracts/FifsUtility.sol
@@ -5,12 +5,16 @@ import "./Utility.sol";
 
 contract FifsUtility is Utility {
 
-  // Number of successful settlements done
-  // Used for backtracking
+  // number of successful settlements done
+  // used for backtracking
   uint256 public checkpoint;
 
-  // Iterable list of all current households
+  // iterable list of all households
   address[] householdList;
+  // iterable list of all current households with positive amount of energy
+  address[] householdListWithEnergy;
+  // iterable list of all current households with negative amount of energy
+  address[] householdListNoEnergy;
 
   struct Deed {
     bool active;
@@ -33,6 +37,38 @@ contract FifsUtility is Utility {
   }
 
   function settle() external returns (bool) {
+    require(totalRenewableEnergy > 0, "No renewable energy available for settlement.");
+
+    // restructure this in seperate function ?
+    // total amount of renewable energy for settlement
+    uint256 availableRenewableEnergy;
+
+    // group households into households sending renewable energy and households recievin renewable energy
+    for (uint256 i = 0; i < householdList.length; i++) {
+      if (households[householdList[i]].renewableEnergy + households[householdList[i]].nonRenewableEnergy <= 0) {
+        // sum of renewable and non-renewable energy is negative or zero; no energy for settlement
+        householdListNoEnergy.push(householdList[i]);
+      } else {
+        // sum of renewable and non-renewable energy is positive
+        if (households[householdList[i]].renewableEnergy > 0 && households[householdList[i]].nonRenewableEnergy > 0) {
+          // both amounts of renewable and non-renewable energy are positive
+          householdListWithEnergy.push(householdList[i]);
+          // add amount of renewable energy to available energy for settlement
+          availableRenewableEnergy = uint256(households[householdList[i]].renewableEnergy);
+        } else if (households[householdList[i]].renewableEnergy > 0 && households[householdList[i]].nonRenewableEnergy < 0) {
+          // amount of renewable energy is positive
+          householdListWithEnergy.push(householdList[i]);
+          // add gap between renewable and non-renewable energy to available energy for settlement
+          availableRenewableEnergy = uint256(households[householdList[i]].renewableEnergy - households[householdList[i]].nonRenewableEnergy);
+        }
+      }
+    }
+
+    // clean setup for next settlement
+    delete availableRenewableEnergy;
+    delete householdListNoEnergy;
+    delete householdListWithEnergy;
+
     checkpoint += 1;
     return true;
   }
