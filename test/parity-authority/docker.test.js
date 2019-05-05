e84e10d1b658b9f7efdc14eb9917a443c4b39510
dakingha69 <dongha.kim210@gmail.com>
2019-05-05 23:28:28 +0200

    Refactor docker and native tests
    The tests now use proper async / await syntax.

--- test/parity-authority/docker.test.js
+++ test/parity-authority/docker.test.js
@@ -5,11 +5,11 @@ const util = require("util");
 const nodes = [process.env.NODE_0, process.env.NODE_1, process.env.NODE_2];
 let testTitle;
 
-describe("Test Node Properties", function() {
-  for (let i = 0; i < nodes.length; i++) {
+describe("Test Node Properties", () => {
+  nodes.forEach((node, i) => {
     testTitle = util.format("Test Node #%d", i);
-    describe(testTitle, function() {
-      const res = request("POST", nodes[i], {
+    describe(testTitle, async () => {
+      const res = await request("POST", nodes[i], {
         json: {
           jsonrpc: "2.0",
           method: "personal_listAccounts",
@@ -26,25 +26,15 @@ describe("Test Node Properties", function() {
         assert.strictEqual(body["result"].length, 1);
       });
     });
-  }
+  });
 });
 
-describe("Test Network Properties", function() {
-  for (let i = 0; i < nodes.length; i++) {
+describe("Test Network Properties", () => {
+  nodes.forEach((node, i) => {
     testTitle = util.format("Test Node #%d", i);
-    describe(testTitle, function() {
-      // delay in each retry after first failure
-      beforeEach(function(done) {
-        if (this.currentTest.currentRetry() > 0) {
-          setTimeout(done, this.currentTest.currentRetry() * 500);
-        } else {
-          done();
-        }
-      });
-      this.retries(10);
-
-      it("Node have two peers", function() {
-        const res = request("POST", nodes[i], {
+    describe(testTitle, () => {
+      it("Node have two peers", async () => {
+        const res = await request("POST", nodes[i], {
           json: {
             jsonrpc: "2.0",
             method: "net_peerCount",
@@ -57,8 +47,8 @@ describe("Test Network Properties", function() {
         assert.strictEqual(body["result"], "0x2");
       });
 
-      it("Block number is higher than 0", function() {
-        const res = request("POST", nodes[i], {
+      it("Block number is higher than 0", async () => {
+        const res = await request("POST", nodes[i], {
           json: {
             jsonrpc: "2.0",
             method: "eth_blockNumber",
@@ -71,5 +61,5 @@ describe("Test Network Properties", function() {
         assert.ok(body["result"] > 0);
       });
     });
-  }
+  });
 });
--- test/parity-authority/native.test.js
+++ test/parity-authority/native.test.js
@@ -2,12 +2,13 @@ const assert = require("assert");
 const request = require("sync-request");
 const util = require("util");
 
+const nodes = [0, 1];
 let title;
 
-describe("Test Node Properties", function() {
-  for (let i = 0; i < 2; i++) {
+describe("Test Node Properties", () => {
+  nodes.forEach(i => {
     title = util.format("Test Node #%d", i);
-    describe(title, function() {
+    describe(title, () => {
       const res = request("POST", "http://localhost:854" + i, {
         json: {
           jsonrpc: "2.0",
@@ -17,19 +18,19 @@ describe("Test Node Properties", function() {
         }
       });
       const body = JSON.parse(res.getBody("utf8"));
-      it("Return response code 200", function() {
+      it("Return response code 200", () => {
         assert.strictEqual(res.statusCode, 200);
       });
 
-      it("Node should have one account", function() {
+      it("Node should have one account", () => {
         assert.strictEqual(body["result"].length, 1);
       });
     });
-  }
+  });
 });
 
-describe.only("Test Network Properties", function() {
-  for (let i = 0; i < 2; i++) {
+describe("Test Network Properties", () => {
+  nodes.forEach(i => {
     title = util.format("Test Node #%d", i);
     describe(title, () => {
       it("Node have one peer", async () => {
@@ -46,7 +47,7 @@ describe.only("Test Network Properties", function() {
         assert.strictEqual(body["result"], "0x1");
       });
 
-      it.skip("Block number is higher than 0", function() {
+      it.skip("Block number is higher than 0", () => {
         const res = request("POST", "http://localhost:854" + i, {
           json: {
             jsonrpc: "2.0",
@@ -60,5 +61,5 @@ describe.only("Test Network Properties", function() {
         assert.ok(body["result"] > 0);
       });
     });
-  }
+  });
 });
