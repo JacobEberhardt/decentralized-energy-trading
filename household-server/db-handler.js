b38de3960c5499900453f30166931f0ea2bac91d
julia832 <julia.holz@live.de>
2019-05-09 19:48:56 +0200

    Fix bug in db-handler.js
    - fix style
    - fix callback handling error

--- household-server/db-handler.js
+++ household-server/db-handler.js
@@ -36,7 +36,7 @@ module.exports = {
         console.log("1 document inserted: ", data);
         db.close();
 
-        return true; 
+        return true;
       });
     });
   },
@@ -45,7 +45,7 @@ module.exports = {
    * Method to read data from the database
    * Should be added as an Eventlistener for incoming GET Requests from the UI
    * @param {String} url URL/URI of the DB
-   * @returns {Array} Result as Array of JSONObjects 
+   * @returns {Array} Result as Array of JSONObjects
    */
   readAll: url => {
     MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
@@ -63,24 +63,35 @@ module.exports = {
         });
     });
   },
-   /**
-    * Method to read data from the database filtered by ID
-    * @param {String} url URL/URI of the DB
-    * @param {Number} id id of the Document
-    * @returns {JSONObject} Result as JSONObject 
-    */
+  /**
+   * Method to read data from the database filtered by ID
+   * @param {String} url URL/URI of the DB
+   * @param {Number} id id of the Document
+   * @returns {JSONObject} Result as JSONObject
+   */
   findByID: (url, id) => {
     MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
       if (err) throw err;
       const dbo = db.db("sensordata");
       dbo
         .collection("data")
-        .findOne({"_id": id}).then(result => {
-          console.log(result.produce, result.consume, result._id, result._id.getTimestamp().toString());
+        .findOne({ _id: id })
+        .then(result => {
+          console.log(
+            result.produce,
+            result.consume,
+            result._id,
+            result._id.getTimestamp().toString()
+          );
           db.close();
 
           return result;
-        }).catch( err => console.log('Entry with id: ',id, "not found"));
+        })
+        .catch(err => {
+          if (err) {
+          }
+          console.log("Entry with id: ", id, "not found");
+        });
     });
   }
 };
