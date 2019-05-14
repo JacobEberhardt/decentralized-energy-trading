03a325a7a6c8515f5ba59b3dd1d6c414a9e8b65e
dakingha69 <dongha.kim210@gmail.com>
2019-05-15 00:29:49 +0200

    Add custom deploy script for authority

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,31 @@
 const Utility = artifacts.require("Utility");
 
+const web3Helper = require("../helpers/web3");
+const authorityHelper = require("../helpers/authority");
+
+const {
+  UTILITY_ADDRESS_IN_AUTHORITY,
+  AUTHORITY_ADDRESS
+} = require("../helpers/constants");
+
 module.exports = async (deployer, network, [authority]) => {
   switch (network) {
     case "ganache": {
       await deployer.deploy(Utility);
       const utilityInstance = await Utility.deployed();
       await utilityInstance.addHousehold(authority);
+      break;
+    }
+    case "authority": {
+      const fifsUtilityInstanceInAuthority = await FifsUtility.at(
+        UTILITY_ADDRESS_IN_AUTHORITY
+      );
+      const web3 = web3Helper.initWeb3("authority");
+      const { address, password } = authorityHelper.getAddressAndPassword();
+      await web3.eth.personal.unlockAccount(address, password, null);
+      await fifsUtilityInstanceInAuthority.addHousehold(AUTHORITY_ADDRESS, {
+        from: AUTHORITY_ADDRESS
+      });
       break;
     }
     default: {
