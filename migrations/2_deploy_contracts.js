60b814b42c25f17a09e2ddd49a234f0f96ab0959
dakingha69 <dongha.kim210@gmail.com>
2019-05-14 13:15:14 +0200

    Change if else to switch in migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,16 @@
 const Utility = artifacts.require("Utility");
 
 module.exports = async (deployer, network, [authority]) => {
-  if (network === "ganache") {
-    await deployer.deploy(Utility);
-    const utilityInstance = await Utility.deployed();
-    await utilityInstance.addHousehold(authority);
-  } else {
-    deployer.deploy(Utility);
+  switch (network) {
+    case "ganache": {
+      await deployer.deploy(Utility);
+      const utilityInstance = await Utility.deployed();
+      await utilityInstance.addHousehold(authority);
+      break;
+    }
+    default: {
+      deployer.deploy(Utility);
+      break;
+    }
   }
 };
