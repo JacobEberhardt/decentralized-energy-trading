0e8955b764f23a9b12e290e615f084e309f97156
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2019-05-16 20:38:20 +0200

    Add first draft of mock sensor
    - Add SensorIntervall in config
    - Add Mock-Sensor
    - Currently throwing "EPROTO" Error

--- mock-sensor/index.js
+++ mock-sensor/index.js
@@ -1,2 +1,43 @@
-// TODO
-console.log("mock sensor");
+const { host, port, sensorInterval } = require("../household-server-config");
+const https = require("https");
+
+console.log(
+  `Starting Mock-Sensor\nSending to http://${host}:${port} with an interval of ${sensorInterval}ms`
+);
+
+const data = (samples, min, max) => {
+  let mockData = [];
+  for (let i = 0; i < samples; i++) {
+    let rndm = Math.random() * (max - min) + min;
+    // rounding the samples
+    rndm = Math.round(rndm * 100) / 100;
+    mockData.push(rndm);
+  }
+  return mockData;
+};
+
+const options = {
+  hostname: host,
+  port: port,
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+
+setInterval((host, port) => {
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", d => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on("error", error => {
+    console.error(error);
+  });
+  console.log("Sending data");
+  req.write(JSON.stringify(data(2, 0, 100)));
+  req.end();
+}, 5000);
