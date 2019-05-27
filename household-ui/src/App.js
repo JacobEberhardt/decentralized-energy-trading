4f1368576ca92b61e35b1f6c327d81c35abbdc4f
dakingha69 <dongha.kim210@gmail.com>
2019-05-27 15:14:39 +0200

    Change location of fetching household/network stats to App

--- household-ui/src/App.js
+++ household-ui/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grommet, Box } from "grommet";
 import { grommet } from "grommet/themes";
 
@@ -8,7 +8,35 @@ import NetworkStats from "./components/NetworkStats";
 import DeedsTicker from "./components/DeedsTicker";
 import Savings from "./components/Savings";
 
+import { fetchFromEndpoint } from "./helpers/fetch";
+
 function App() {
+  const [householdStats, setHouseholdStats] = useState({
+    address: "",
+    producedRenewableEnergy: 0,
+    consumedRenewableEnergy: 0,
+    renewableEnergy: 0
+  });
+  const [networkStats, setNetworkStats] = useState({
+    totalProducedRenewableEnergy: 0,
+    totalEnergy: 0,
+    totalConsumedRenewableEnergy: 0
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const [fetchedHouseholdStats, fetchedNetworkStats] = await Promise.all([
+        fetchFromEndpoint(`/household-stats`),
+        fetchFromEndpoint(`/network-stats`)
+      ]);
+      setHouseholdStats(fetchedHouseholdStats);
+      setNetworkStats(fetchedNetworkStats);
+    };
+    fetchData();
+    const interval = setInterval(() => fetchData(), 10000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Grommet theme={grommet}>
       <TopNav />
@@ -20,7 +48,14 @@ function App() {
         background={{ color: "light-2" }}
       >
         <SensorStats />
-        <NetworkStats />
+        <NetworkStats
+          producedEnergyByHousehold={householdStats.producedRenewableEnergy}
+          consumedEnergyByHousehold={householdStats.consumedRenewableEnergy}
+          balanceOfHousehold={householdStats.renewableEnergy}
+          producedEnergyByNetwork={networkStats.totalProducedRenewableEnergy}
+          consumedEnergyByNetwork={networkStats.totalConsumedRenewableEnergy}
+          balanceOfNetwork={networkStats.totalEnergy}
+        />
         <DeedsTicker />
         <Savings />
       </Box>
