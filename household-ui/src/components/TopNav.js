617976884390cc9c4f5fbe510dab81d7eaa1b712
dakingha69 <dongha.kim210@gmail.com>
2019-05-27 12:28:17 +0200

    Add address to TopNav component

--- household-ui/src/components/TopNav.js
+++ household-ui/src/components/TopNav.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Text } from "grommet";
 
+import { fetchFromEndpoint } from "../helpers/fetch";
+
 const TopNav = () => {
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    async function fetchAddress() {
+      const { address } = await fetchFromEndpoint("/household-stats");
+      setAddress(address);
+    }
+    fetchAddress();
+  }, []);
+
   return (
-    <Box fill direction={"row"} pad={"small"} background={"brand"}>
+    <Box fill direction={"row"} pad={"small"} background={"brand"} justify={"between"}>
       <Text>Decentralized Energy Trading</Text>
+      <Text>{address}</Text>
     </Box>
   );
 };
