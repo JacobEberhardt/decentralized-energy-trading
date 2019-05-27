b952485f98fd91a7475daa357c03e1107d18ef5f
dakingha69 <dongha.kim210@gmail.com>
2019-05-27 17:37:33 +0200

    Add legends to sensor stats

--- household-ui/src/components/SensorStats.js
+++ household-ui/src/components/SensorStats.js
@@ -7,7 +7,8 @@ import {
   VerticalGridLines,
   HorizontalGridLines,
   XAxis,
-  YAxis
+  YAxis,
+  DiscreteColorLegend
 } from "react-vis";
 
 import DashboardBox from "./DashboardBox";
@@ -31,21 +32,37 @@ const formatConsumeData = rawData => {
 };
 
 const SensorStats = React.memo(({ sensorData }) => {
+  const range = sensorData
+    .map(({ consume, produce }) => [consume, produce])
+    .flat();
   return (
     <DashboardBox title={"Sensor Data"}>
       <Box>
-        <XYPlot height={400} width={700}>
+        <DiscreteColorLegend
+          colors={["green", "red"]}
+          items={["produced energy", "consumed energy"]}
+          orientation="horizontal"
+        />
+        <XYPlot
+          height={350}
+          width={700}
+          yDomain={[0, Math.max(...range) * 1.3]}
+        >
           <VerticalGridLines />
           <HorizontalGridLines />
           <LineSeries
             data={formatProduceData(sensorData)}
             color={"green"}
             strokeStyle="solid"
+            strokeWidth={3}
+            opacity={0.5}
           />
           <LineSeries
             data={formatConsumeData(sensorData)}
             color={"red"}
             strokeStyle="solid"
+            strokeWidth={3}
+            opacity={0.5}
           />
           <XAxis
             title={"time"}
