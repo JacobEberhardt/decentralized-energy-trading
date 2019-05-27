fee3ada1e6c13da612eee811f56e571cb98bc319
Pablo Osinaga <paguos@gmail.com>
2019-05-27 03:08:49 +0200

    [UC] Gas estimation for Utility Contract
    - Add csv-writter dependency
    - Add utility-benchmark yarn script
    - Update README.md and .gitignore

---
+++ scripts/benchmarks/utility_benchmark.js
@@ -0,0 +1,60 @@
+const root = process.env.PROJECT_ROOT;
+const web3Helper = require(`${root}/helpers/web3`);
+const authorityHelper = require(`${root}/helpers/authority`);
+const contractData = require(`${root}/build/contracts/UtilityBenchmark.json`);
+const web3 = web3Helper.initWeb3("authority");
+const { address, password } = authorityHelper.getAddressAndPassword();
+
+var data = [];
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
+async function generate_csv() {
+  console.log("Generating CSV file ...");
+  const csvWriter = createCsvWriter({
+    path: "utility_benchmark.csv",
+    header: [
+      { id: "households", title: "Nr. of Households" },
+      { id: "gas", title: "Estimated Gas" },
+      { id: "time", title: "Estimation Time (ms)" },
+    ],
+  });
+  await csvWriter.writeRecords(data);
+  console.log("CSV file was generated successfully!");
+}
+
+async function utility_benchmark(households, maxHouseholds) {
+  console.log("Starting Utility Benchmark ...");
+
+  while (households < maxHouseholds) {
+    console.log(`Deploying UtilityBenchmark Contract for ${households} Households ...`);
+
+    await web3.eth.personal.unlockAccount(address, password, null);
+    const contract = new web3.eth.Contract(contractData.abi);
+    const deployedContract = await contract
+      .deploy({
+        data: contractData.bytecode,
+        arguments: [households, households, 100, -100],
+      })
+      .send({
+        from: address,
+        gas: 200000000000000,
+        gasPrice: "300000000000",
+      })
+      .catch(err => console.log(err));
+
+    console.log("UtilityBenchmark Contract Address: " + deployedContract.address);
+    var start = new Date();
+    await deployedContract.methods
+      .settle()
+      .estimateGas({ gas: 3061084102 }, function(error, gasAmount) {
+        console.log(`Gas: ${gasAmount}`);
+        var end = new Date();
+        data.push({ households: households, gas: gasAmount, time: end - start });
+      });
+    households = households * 2;
+  }
+  await generate_csv();
+  console.log("Utility Benchmark completed!");
+}
+
+utility_benchmark(25, 26000);
