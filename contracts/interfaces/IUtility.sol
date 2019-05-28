91ed2c1828c9638749ceee7ccef7d8337a0650c7
Myron Rotter <myronrotter01@t-online.de>
2019-05-28 19:54:05 +0200

    Add events `RequestNonRenewableEnergy` and `EnergyCompensated`

--- contracts/interfaces/IUtility.sol
+++ contracts/interfaces/IUtility.sol
@@ -7,6 +7,11 @@ import "./IUtilityBase.sol";
  * @title Utility interface
  */
 contract IUtility is IUtilityBase {
+
+  event RequestNonRenewableEnergy(address indexed household, int256 energy);
+
+  event EnergyCompensated(address household, int256 energy, bool isRenewable);
+
   function settle() external returns (bool);
 
   function deedsLength(uint256 _blockNumber) public view returns (uint256);
