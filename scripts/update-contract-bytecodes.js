97b7f61e72442a26f25a60c82fbf19447197efa5
dakingha69 <dongha.kim210@gmail.com>
2019-06-01 13:34:36 +0200

    Add script to update contract bytecides in chain spec

---
+++ scripts/update-contract-bytecodes.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+
+const chainSpec = require("../parity-authority/docker/parity/config/chain.json");
+const utilityArtefact = require("../build/contracts/Utility.json");
+const blockRewardArtefact = require("../build/contracts/BlockReward.json");
+const validatorSetArtefact = require("../build/contracts/OwnedSet.json");
+
+const UTILITY_ADDRESS = "0x0000000000000000000000000000000000000042";
+const BLOCK_REWARD_ADDRESS = "0x0000000000000000000000000000000000000043";
+const VALIDATOR_SET_ADDRESS = "0x0000000000000000000000000000000000000044";
+// TODO: Dynamically generate with web3
+const ENCODED_INITIAL_VALIDATOR_ADDRESSES =
+  "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000bd138abd70e2f00903268f3db08f2d25677c9e00000000000000000000000000aa39d30f0d20ff03a22ccfc30b7efbfca597c2000000000000000000000000002e28950558fbede1a9675cb113f0bd20912019";
+
+if (!utilityArtefact) {
+  throw new Error("No contract artefact for Utility found.");
+}
+
+if (!blockRewardArtefact) {
+  throw new Error("No contract artefact for BlockReward found.");
+}
+
+if (!validatorSetArtefact) {
+  throw new Error("No contract artefact for ValidatorSet found.");
+}
+
+fs.writeFile(
+  path.resolve("./parity-authority/docker/parity/config/chain.json"),
+  JSON.stringify(
+    {
+      ...chainSpec,
+      accounts: {
+        ...chainSpec.accounts,
+        [UTILITY_ADDRESS]: {
+          balance: 1,
+          constructor: utilityArtefact.bytecode
+        },
+        [BLOCK_REWARD_ADDRESS]: {
+          balance: 1,
+          constructor: blockRewardArtefact.bytecode
+        },
+        [VALIDATOR_SET_ADDRESS]: {
+          balance: 1,
+          constructor:
+            validatorSetArtefact.bytecode + ENCODED_INITIAL_VALIDATOR_ADDRESSES
+        }
+      }
+    },
+    null,
+    2
+  ),
+  err => {
+    if (err) {
+      throw err;
+    }
+    console.log("Contract bytecodes updated.");
+  }
+);
