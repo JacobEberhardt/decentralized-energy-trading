1eb549088d902c4284d6f27d8c9705c545f6f6c4
Simon <s.huber-og@web.de>
2019-06-12 16:34:46 +0200

    Add basic zk-handler
    No logic here. just a template

---
+++ ned-server/zk-handler.js
@@ -0,0 +1,9 @@
+/**
+ * This handler manages the communication of the NED Server and the ZoKrates environment
+ */
+module.exports = {
+  // TODO trigger the netting by calling the zokrates bash file
+  triggerNetting: async householdTransaction => {
+    return true;
+  }
+};
