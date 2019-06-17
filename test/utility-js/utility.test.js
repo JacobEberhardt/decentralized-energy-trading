994d201c7418aaa479f799684dfbf115ff69ca58
Myron Rotter <myronrotter01@t-online.de>
2019-06-17 14:06:17 +0200

    Fix lint error; remove redundancy of energy type

--- test/utility-js/utility.test.js
+++ test/utility-js/utility.test.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
 const Utility = require("../../utility-js/Utility");
 
 const expect = require("chai").expect;
+
+const RENEWABLE_ENERGY = "renewableEnergy";
+const NONRENEWABLE_ENERGY = "nonRenewableEnergy";
 
 describe("Utility", () => {
   const hhAddress = "0x45D4b6e19b3fee56bA93972d8d72aC65FeF26b31";
@@ -14,8 +19,8 @@ describe("Utility", () => {
     });
 
     it("is initialized correctly", () => {
-      expect(Utility.households[hhAddress].renewableEnergy).to.equal(0);
-      expect(Utility.households[hhAddress].nonRenewableEnergy).to.equal(0);
+      expect(Utility.households[hhAddress][RENEWABLE_ENERGY]).to.equal(0);
+      expect(Utility.households[hhAddress][NONRENEWABLE_ENERGY]).to.equal(0);
     });
 
     it("reverts when attempting to add an existing household", () => {
@@ -26,14 +31,14 @@ describe("Utility", () => {
   describe("Update energy", () => {
     it("updateRenewableEnergy by 100", () => {
       Utility.updateRenewableEnergy(hhAddress, 100);
-      expect(Utility.households[hhAddress].renewableEnergy).to.equal(100);
-      expect(Utility.renewableEnergy).to.equal(100);
+      expect(Utility.households[hhAddress][RENEWABLE_ENERGY]).to.equal(100);
+      expect(Utility[RENEWABLE_ENERGY]).to.equal(100);
     });
 
     it("updateNonRenewableEnergy by 100", () => {
       Utility.updateNonRenewableEnergy(hhAddress, 100);
-      expect(Utility.households[hhAddress].nonRenewableEnergy).to.equal(100);
-      expect(Utility.nonRenewableEnergy).to.equal(100);
+      expect(Utility.households[hhAddress][NONRENEWABLE_ENERGY]).to.equal(100);
+      expect(Utility[NONRENEWABLE_ENERGY]).to.equal(100);
     });
 
     it("reverts when attempting to update energy of not existing household", () => {
@@ -41,7 +46,7 @@ describe("Utility", () => {
         Utility._updateEnergy(
           "0x45D4b6e19b3fee56bA93972d8d72aC65FeF26b32",
           100,
-          "renewableEnergy"
+          RENEWABLE_ENERGY
         )
       ).to.be.false;
     });
