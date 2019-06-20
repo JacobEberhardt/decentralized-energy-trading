c8b712df68ddcfce99330de704ffaa8c806f297d
Duy Minh Vo <vminhd@gmail.com>
2019-06-20 22:24:55 +0200

    (WIP) Refactor Utility.sol as an onchain settlement verifier
    WARNING: mostly untested code. Might be unstable.

--- contracts/interfaces/IUtility.sol
+++ contracts/interfaces/IUtility.sol
@@ -8,11 +8,15 @@ import "./IUtilityBase.sol";
  */
 contract IUtility is IUtilityBase {
 
-  event RequestNonRenewableEnergy(address indexed household, int256 energy);
+  event NettingSuccess(uint blockNumber);
 
-  event EnergyCompensated(address household, int256 energy, bool isRenewable);
+  function setVerifier(address _verifier) external returns (bool);
 
-  function settle() external returns (bool);
+  function verifyNetting(
+    uint256[2] calldata _a,
+    uint256[2][2] calldata _b,
+    uint256[2] calldata _c,
+    uint256[1] calldata _input) external returns (bool success);
 
-  function deedsLength(uint256 _blockNumber) public view returns (uint256);
+  function getDeedsLength() external view returns (uint256);
 }
--- contracts/interfaces/IVerifier.sol
+++ contracts/interfaces/IVerifier.sol
@@ -1,8 +1,5 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./IUtilityBase.sol";
-
-
 /**
  * @title Verifier contract interface
  */
