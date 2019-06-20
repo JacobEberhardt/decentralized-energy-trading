977d023682481764f87686cfce2379f9e1ecb641
Duy Minh Vo <vminhd@gmail.com>
2019-06-20 22:22:45 +0200

    Add IVerifier.sol interface
    So we can utilize its ABI to call verifyTx() within Utility.sol

---
+++ contracts/interfaces/IVerifier.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./IUtilityBase.sol";
+
+
+/**
+ * @title Verifier contract interface
+ */
+contract IVerifier {
+  function verifyTx(
+    uint[2] memory a,
+    uint[2][2] memory b,
+    uint[2] memory c,
+    uint[1] memory input) public returns (bool);
+}
