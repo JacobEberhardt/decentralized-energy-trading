05db9f3588caf3672079c43d86a38cb98805f553
dakingha69 <dongha.kim210@gmail.com>
2019-06-22 14:12:51 +0200

    [HHS] Add dedicated handlers for deeds and energy

---
+++ household-server/deed-handler.js
@@ -0,0 +1,35 @@
+const db = require("./apis/db");
+const ned = require("./apis/ned");
+
+module.exports = {
+  /**
+   * Collects deeds from NED sever and writes them into DB.
+   * @param {{
+   *   host: string,
+   *   port: number,
+   *   dbUrl: string,
+   *   nedUrl: string,
+   *   network: string,
+   *   address: string,
+   *   password: string,
+   *   dbName: string,
+   *   sensorDataCollection: string,
+   *   utilityDataCollection: string
+   * }} config Server configuration
+   */
+  collectDeeds: async config => {
+    const latestSavedTimestamp = await db.getLatestTimestamp(
+      config.dbUrl,
+      config.dbName,
+      config.collection
+    );
+    const deeds = await ned.getDeeds(
+      config.nedUrl,
+      config.address,
+      latestSavedTimestamp + 1
+    );
+    return deeds.length > 0
+      ? db.bulkWriteToDB(config.dbUrl, config.dbName, config.collection, deeds)
+      : [];
+  }
+};
---
+++ household-server/energy-handler.js
@@ -0,0 +1,31 @@
+const authority = require("./apis/authority");
+const ned = require("./apis/ned");
+
+module.exports = {
+  /**
+   * Collects deeds from NED sever and writes them into DB.
+   * @param {{
+   *   host: string,
+   *   port: number,
+   *   dbUrl: string,
+   *   nedUrl: string,
+   *   network: string,
+   *   address: string,
+   *   password: string,
+   *   dbName: string,
+   *   sensorDataCollection: string,
+   *   utilityDataCollection: string
+   * }} config Server configuration.
+   * @param {Object} web3 Web3 instance.
+   * @param {number} energyDelta Delta of produced and consumed.
+   */
+  putEnergy: async (config, web3, energyDelta) => {
+    const signedData = authority.signData(
+      web3,
+      config.address,
+      config.password,
+      energyDelta
+    );
+    return ned.putEnergy(config.nedUrl, config.address, signedData);
+  }
+};
