c7c2c2aa160f3e4278c6337b444593caacce34c3
Duy Minh Vo <vminhd@gmail.com>
2019-06-29 23:18:49 +0200

    Reintroduce household management and tracking
    - With the difference that we store energy balance change hashes
    - Implement removeHousehold()

--- contracts/dUtility.sol
+++ contracts/dUtility.sol
@@ -6,14 +6,72 @@ import "./Mortal.sol";
 
 /**
  * @title Utility onchain settlement verifier.
- * @dev Inherits from UtilityBase.
- * NOTE: this contract stays as 'Utility.sol' for compatibility reasons ;)
+ * @dev Inherits from IdUtility. This approach is analoguous to UtilityBase.sol but with
+ * private energy state changes.
  */
 contract dUtility is Mortal, IdUtility {
 
-  IVerifier private verifier;
+  struct Household {
+    // for checks if household exists
+    bool initialized;
+
+    // Hashes of (deltaEnergy+nonce+msg.sender)
+    bytes32 renewableEnergy;
+    bytes32 nonRenewableEnergy;
+  }
+
+  // mapping of all households
+  mapping(address => Household) households;
+
+  modifier onlyHousehold(address _household) {
+    require(msg.sender == _household, "No permission to access. Only household may access itself.");
+    _;
+  }
+
+  modifier householdExists(address _household) {
+    require(households[_household].initialized, "Household does not exist.");
+    _;
+  }
 
   uint256[] public deeds;
+
+  IVerifier private verifier;
+
+  /**
+   * @dev Create a household with address _household to track energy production and consumption.
+   * Emits NewHousehold when household was added successfully.
+   * @param _household address of the household
+   * @return success bool if household does not already exists, should only be called by some authority
+   */
+  function addHousehold(address _household) external onlyOwner() returns (bool) {
+    return _addHousehold(_household);
+  }
+
+  /**
+   * @dev Get energy properties of _household.
+   * @param _household address of the household
+   * @return Household stats (initialized,
+   *                          renewableEnergy,
+   *                          nonRenewableEnergy)
+   *          of _household if _household exists
+   */
+  function getHousehold(address _household) external view householdExists(_household) returns (bool, bytes32, bytes32) {
+    Household memory hh = households[_household];
+    return (
+      hh.initialized,
+      hh.renewableEnergy,
+      hh.nonRenewableEnergy
+    );
+  }
+
+  /**
+   * @dev Removes a household.
+   * @param _household address of the household
+   * @return success bool if household does not already exists, should only be called by some authority
+   */
+  function removeHousehold(address _household) external onlyOwner() householdExists(_household) returns (bool) {
+    delete households[_household];
+  }
 
   /**
    * @dev Sets the address of a ZoKrates verifier contract.
@@ -46,5 +104,70 @@ contract dUtility is Mortal, IdUtility {
    */
   function getDeedsLength() external view returns (uint256) {
     return deeds.length;
+  }
+
+  /**
+   * @dev Updates a household's renewable energy state calling _updateEnergy
+   * @param _household address of the household
+   * @param _deltaEnergy bytes32 hash of (delta+nonce+senderAddr)
+   * @return success bool returns true, if function was called successfully
+   */
+  function updateRenewableEnergy(address _household, bytes32 _deltaEnergy)
+  external
+  onlyHousehold(_household)
+  returns (bool) {
+    _updateEnergy(_household, _deltaEnergy, true);
+  }
+
+  /**
+   * @dev Updates a household's non-renewable energy state calling _updateEnergy
+   * @param _household address of the household
+   * @param _deltaEnergy bytes32 hash of (delta+nonce+senderAddr)
+   * @return success bool returns true, if function was called successfully
+   */
+  function updateNonRenewableEnergy(address _household, bytes32 _deltaEnergy)
+  external onlyHousehold(_household)
+  returns (bool) {
+    _updateEnergy(_household, _deltaEnergy, false);
+  }
+
+    /**
+   * @dev Updates a household's energy state
+   * @param _household address of the household
+   * @param _deltaEnergy bytes32 hash of (delta+nonce+senderAddr)
+   * @param _isRenewable bool indicates whether said energy is renewable or non-renewable
+   * @return success bool returns true, if function was called successfully
+   */
+  function _updateEnergy(address _household, bytes32 _deltaEnergy, bool _isRenewable)
+  internal
+  householdExists(_household)
+  returns (bool) {
+    Household storage hh = households[_household];
+    if (_isRenewable) {
+      hh.renewableEnergy = _deltaEnergy;
+      emit RenewableEnergyChanged(_household, _deltaEnergy);
+    } else {
+      hh.nonRenewableEnergy = _deltaEnergy;
+      emit NonRenewableEnergyChanged(_household, _deltaEnergy);
+    }
+    return true;
+  }
+
+  /**
+   * @dev see UtilityBase.addHousehold
+   * @param _household address of household
+   * @return success bool
+   */
+  function _addHousehold(address _household) internal onlyOwner returns (bool) {
+    require(!households[_household].initialized, "Household already exists.");
+
+    // add new household to mapping
+    Household storage hh = households[_household];
+    hh.initialized = true;
+    hh.renewableEnergy = 0;
+    hh.nonRenewableEnergy = 0;
+
+    emit NewHousehold(_household);
+    return true;
   }
 }
--- contracts/interfaces/IdUtility.sol
+++ contracts/interfaces/IdUtility.sol
@@ -1,15 +1,27 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./IUtilityBase.sol";
-
-
 /**
- * @title Utility interface
+ * @title dUtility interface
  */
-contract IdUtility is IUtilityBase {
+contract IdUtility {
+
+  /* Events */
+  event NewHousehold(address indexed household);
 
   event NettingSuccess(uint blockNumber);
 
+  event RenewableEnergyChanged(address indexed household, bytes32 newDeltaEnergy);
+
+  event NonRenewableEnergyChanged(address indexed household, bytes32 newDeltaEnergy);
+
+  /* Household management */
+  function addHousehold(address _household) external returns (bool);
+
+  function getHousehold(address _household) external view returns (bool, bytes32, bytes32);
+
+  function removeHousehold(address _household) external returns (bool);
+
+  /* Settlement verification related methods */
   function setVerifier(address _verifier) external returns (bool);
 
   function verifyNetting(
@@ -19,4 +31,9 @@ contract IdUtility is IUtilityBase {
     uint256[1] calldata _input) external returns (bool success);
 
   function getDeedsLength() external view returns (uint256);
+
+  /* dUtility household balance change tracking methods */
+  function updateRenewableEnergy(address _household, bytes32 deltaEnergy) external returns (bool);
+
+  function updateNonRenewableEnergy(address _household, bytes32 deltaEnergy) external returns (bool);
 }
