4722cf35d190ee91580db72acee462b5b290ff59
Pablo Osinaga <paguos@gmail.com>
2019-07-01 11:30:55 +0200

    [NED] Docker for NED instance
    - Restrcuture to NED Procesing Unit
    - Add new star_ned bash script
    - Add parity admin client
    - Update other Docker images to support the new install parity script

---
+++ scripts/start_ned.sh
@@ -0,0 +1,9 @@
+echo "Staring NED Processing Unit ..."
+echo "Staring Parity Node ..."
+parity account list --config parity/authority.toml > authority_address
+parity --config ./parity/authority.toml --engine-signer $(cat authority_address) &> /dev/null
+
+yarn migrate-contracts-authority
+
+echo "Staring Ned Server ..."
+yarn run-ned
