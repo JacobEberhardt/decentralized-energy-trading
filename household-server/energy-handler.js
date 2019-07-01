1a363fafc340b9a7489e6e36ce95e96bf9655493
dakingha69 <dongha.kim210@gmail.com>
2019-07-01 20:42:15 +0200

    Change sha3 to sha256 in energy-handler

--- household-server/energy-handler.js
+++ household-server/energy-handler.js
@@ -1,4 +1,5 @@
 const web3Utils = require("web3-utils");
+const sha256 = require("js-sha256");
 
 const conversionHelper = require("../helpers/conversion");
 const web3Helper = require("../helpers/web3");
@@ -25,11 +26,11 @@ module.exports = {
   putSignedEnergy: async (config, web3, energy) => {
     const { address, password } = config;
     const timestamp = Date.now();
-    const hash = web3Utils.soliditySha3(
-      conversionHelper.kWhToWs(energy),
-      address,
-      timestamp
+    const encodedParams = web3.eth.abi.encodeParameters(
+      ["int256", "uint64", "address"],
+      [conversionHelper.kWhToWs(energy), timestamp, address]
     );
+    const hash = sha256(web3Utils.padLeft(encodedParams, 512));
     const { signature, signerAddress } = await web3Helper.signData(
       web3,
       address,
