846676c883ddec31c5aae9994e8ea74053835076
Paul E <etscheitp@gmail.com>
2019-11-01 14:40:37 +0100

    fixes checkHashes

--- helpers/zokrates.js
+++ helpers/zokrates.js
@@ -5,6 +5,6 @@ module.exports = {
   padPackParams256: (meterDelta) => {
     const meterDeltaHex = web3Utils.numberToHex(meterDelta);
     // Pad concatenated hex params string to have length of 512 bits (128 hex digits).
-    return web3Utils.padLeft(meterDeltaHex, 128);
+    return web3Utils.padLeft(meterDeltaHex, 64);
   }
 };
