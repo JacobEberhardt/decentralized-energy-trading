eeabbba4e6b85721e3097ae50ea1e2273116cc2f
Paul E <etscheitp@gmail.com>
2019-11-22 15:26:25 +0100

    new utility distibution function

---
+++ utility-js/test.js
@@ -0,0 +1,113 @@
+// TESTS:
+
+// let hhFrom = [{ meterDelta: 200 }, { meterDelta: 400 }]
+// let hhTo = [{ meterDelta: -130 }, {meterDelta: -270}]
+// let deltaProducers = 600;
+// let deltaConsumers = 400;
+// Expected:
+//  prod: 67, 133
+//  cons: 0, 0
+
+// let hhFrom = [{ meterDelta: 100 }, { meterDelta: 200 }]
+// let hhTo = [{ meterDelta: -200 }, { meterDelta: -200 }]
+// let deltaProducers = 300;
+// let deltaConsumers = 400;
+// // Expected:
+// //  prod: 0, 0
+// //  cons: -50, -50
+
+// let hhFrom = [{ meterDelta: 100 }, { meterDelta: 900 }]
+// let hhTo = [{ meterDelta: -10 }, { meterDelta: -90 }]
+// let deltaProducers = 1000;
+// let deltaConsumers = 100;
+// Expected:
+//  prod: 90, 810
+//  cons: 0, 0
+
+// let hhFrom = [{ meterDelta: 0 }, { meterDelta: 9 }]
+// let hhTo = [{ meterDelta: -10 }, { meterDelta: -90 }]
+// let deltaProducers = 9;
+// let deltaConsumers = 100;
+// Expected:
+//  prod: 0, 0
+//  cons: -9, -82
+
+
+function _proportionalDistributionn(deltaProducers, deltaConsumers, hhFrom, hhTo) {
+    console.log(hhFrom)
+    console.log(hhTo);
+    const isMoreAvailableThanDemanded = deltaProducers > deltaConsumers;
+
+    const factorConsumers = hhTo.map(obj => Math.round((Math.abs(obj.meterDelta) / deltaConsumers) * 10000) / 10000);
+    const energyReference = isMoreAvailableThanDemanded ? deltaProducers : deltaConsumers;
+
+    for (let i = 0; i < hhFrom.length; i++) {
+        let proportionalFactor = hhFrom[i].meterDelta / energyReference;
+        let shareProducer = Math.round(Math.abs(deltaConsumers) * proportionalFactor);
+        for(let j = 0; j < hhTo.length; j++){
+            let toTransfer = Math.round(shareProducer * factorConsumers[j])
+            console.log("ShareProducer (", shareProducer, ") * factorConsumers (", factorConsumers[j], ") = Amount => ", toTransfer)
+            transfer(i, j, toTransfer);
+        }
+    }
+}
+
+function transfer(from, to, amount){
+    hhFrom[from].meterDelta -= amount;
+    hhTo[to].meterDelta += amount;
+    console.log(hhFrom)
+    console.log(hhTo);
+
+}
+
+// TESTS:
+// let hhFrom = [{ meterDelta: 200 }, { meterDelta: 400 }]
+// let hhTo = [{ meterDelta: -130 }, {meterDelta: -270}]
+// let deltaProducers = 600;
+// let deltaConsumers = 400;
+// Expected:
+//  prod: 67, 133
+//  cons: 0, 0
+
+// let hhFrom = [{ meterDelta: 100 }, { meterDelta: 200 }]
+// let hhTo = [{ meterDelta: -200 }, { meterDelta: -200 }]
+// let deltaProducers = 300;
+// let deltaConsumers = 400;
+// // Expected:
+// //  prod: 0, 0
+// //  cons: 50, 50
+
+// function settle() {
+//     console.log(hhFrom)
+//     console.log(hhTo);
+//     const isMoreAvailableThanDemanded = deltaProducers > deltaConsumers;
+//     const factorConsumers = hhTo.map(obj => {
+//         return Math.round((Math.abs(obj.meterDelta) / deltaConsumers) * 100000) / 100000 //Javascript being Javascript
+//     })
+
+//     const energyReference = isMoreAvailableThanDemanded ? deltaProducers : deltaConsumers;
+//     const energyToShare = isMoreAvailableThanDemanded ? deltaConsumers : deltaProducers;
+
+//     for (let i = 0; i < hhFrom.length; i++) {
+//         let proportionalFactor = hhFrom[i].meterDelta / energyReference;
+//         let shareProducer = Math.round(Math.abs(energyToShare) * proportionalFactor);
+
+//         console.log("proportionalFactor: ", proportionalFactor)
+//         for (let j = 0; j < hhTo.length; j++) {
+//             let toTransfer = Math.round(shareProducer * factorConsumers[j])
+//             console.log("ShareProducer (", shareProducer, ") * factorConsumers (", factorConsumers[j], ") = Amount => ", toTransfer)
+//             transfer(i, j, toTransfer);
+
+//         }
+//     }
+// }
+
+// function transfer(from, to, amount) {
+//     hhFrom[from].meterDelta -= amount;
+//     hhTo[to].meterDelta += amount;
+//     console.log(hhFrom)
+//     console.log(hhTo);
+// }
+
+// settle()
+settle()
