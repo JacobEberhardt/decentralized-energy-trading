367fdbd2f4d0cb8e8b93ca5102a2b46e3bb0e38c
Paul E <etscheitp@gmail.com>
2019-12-17 14:18:30 +0100

    added helper (needed for new benchmarking script)

--- helpers/zokrates.js
+++ helpers/zokrates.js
@@ -1,10 +1,16 @@
 const web3Utils = require("web3").utils;
-const stripHexPrefix = require("strip-hex-prefix");
+const sha256 = require("js-sha256");
+
 
 module.exports = {
   padPackParams256: (meterDelta) => {
     const meterDeltaHex = web3Utils.numberToHex(meterDelta);
-    // Pad concatenated hex params string to have length of 512 bits (128 hex digits).
     return web3Utils.padLeft(meterDeltaHex, 128);
+  },
+
+  packAndHash: (meterDelta) => {
+    const meterDeltaHex = web3Utils.numberToHex(Math.abs(meterDelta));
+    let meterDeltaPadded = web3Utils.padLeft(meterDeltaHex, 128);
+    return `0x${sha256(web3Utils.hexToBytes(meterDeltaPadded))}`;
   }
 };
