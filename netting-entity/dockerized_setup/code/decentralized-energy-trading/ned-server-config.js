c3fd00c1418aedf659c1b878416e5684217afd15
Umar Hayat Siddiq <siddiq@campus.tu-berlin.de>
2020-02-04 15:29:14 +0100

    Updated NED-Config files regarding working-directory

--- netting-entity/dockerized_setup/code/decentralized-energy-trading/ned-server-config.js
+++ netting-entity/dockerized_setup/code/decentralized-energy-trading/ned-server-config.js
@@ -13,7 +13,7 @@
       // Time Interval of the ned server triggering the netting in the ZoKrates execution environment
       nettingInterval: 10000,
       // Working directory of the file and the child process
-      workingDir: "./ned-server",
+      workingDir: "./netting-entity",
       // File name to execute
       fileName: "helloworld.sh",
       // Execution environment for the file
