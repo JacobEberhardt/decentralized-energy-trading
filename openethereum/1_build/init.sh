ccac0efef6b2cc9864aeb5b7c0b44c7ae9c79e81
nightingale <jkuhlenkamp@users.noreply.github.com>
2020-07-24 08:26:53 +0200

    Cleanup

--- openethereum/1_build/init.sh
+++ openethereum/1_build/init.sh
@@ -11,6 +11,10 @@
 #  BOOTNODE_PORT    RPC port of of a bootnode
 #  CHAIN_NAME       Name of the chain
 #  ENODE            Enode of node
+# TODOs:
+#  Ignore -d in CMD
+#  Ignore -c in CMD
+#  Ignore --chain in CMD
 ##################################################
 
 
@@ -45,7 +49,10 @@ function get_enode {
       sleep 1
       continue
       else
-        ENODE=$result
+        echo "result=$result"
+        ENODE="${result:1:-1}"
+        echo "$ENODE=ENODE"
+        # ENODE=$result
         break
     fi
 
@@ -54,27 +61,48 @@ function get_enode {
 
 echo "Running init.sh"
 
+if [[ -z $NODE_NAME ]]; then
+  NODE_NAME=node0
+fi
+
+if [[ -z $ACCOUNT_PASSWORD ]]; then
+  ACCOUNT_PASSWORD=paSSword
+fi
+
+if [[ -z $CHAIN_NAME ]]; then
+  CHAIN_NAME=blogpv
+fi
+
+# Node password file
+echo "Setting ACCOUNT_PASSWORD=$ACCOUNT_PASSWORD in file /home/blogpv/bootnode_account.pwd ..."
 echo "${ACCOUNT_PASSWORD}" > /home/blogpv/node_account.pwd
+
+echo "/home/blogpv/node_account.pwd contains: $(cat /home/blogpv/node_account.pwd)"
 # TODO SET ACCESS RIGHT!
 # TODO DELETE PASSWORD FROM MEMORY!
 
-# Node account
-# echo "Creating account for ACCOUNT_PASSWORD=$ACCOUNT_PASSWORD ..."
+# Authority account
+echo "Creating account for ACCOUNT_PASSWORD=$ACCOUNT_PASSWORD ..."
 rm -Rf /home/openethereum/.local/share/io.parity.ethereum/keys/ethereum
 ACCOUNT_ADDRESS=$(/home/openethereum/openethereum account new --password /home/blogpv/node_account.pwd)
 ACCOUNT_KEYFILE_NAME=$(ls /home/openethereum/.local/share/io.parity.ethereum/keys/ethereum/)
-# echo "ACCOUNT_ADDRESS=$ACCOUNT_ADDRESS"
-# echo "Keyfile: $ACCOUNT_KEYFILE_NAME"
+echo "ACCOUNT_ADDRESS=$ACCOUNT_ADDRESS"
+echo "Keyfile: $ACCOUNT_KEYFILE_NAME"
+cat /home/openethereum/.local/share/io.parity.ethereum/keys/ethereum/$ACCOUNT_KEYFILE_NAME
 
-# echo "bootnode_address: $(cat /home/blogpv/bootnode_address)"
+# Node key
+mkdir -p /home/blogpv/network
 
 # Chain config
 echo "Creating node config from /home/blogpv/template-chain.json ..."
-echo "Setting ACCOUNT_ADDRESS=$ACCOUNT_ADDRESS in file /home/blogpv/chain.json ..."
+# echo "Setting ACCOUNT_ADDRESS=$ACCOUNT_ADDRESS in file /home/blogpv/chain.json ..."
 sed \
 -e "s/{ ACCOUNT_ADDRESS }/$ACCOUNT_ADDRESS/" \
 -e "s/{ CHAIN_NAME }/$CHAIN_NAME/" \
 /home/blogpv/template-chain.json > /home/blogpv/chain.json
+
+# echo "Chain config - /home/blogpv/chain.json:"
+# echo "$(cat /home/blogpv/chain.json)"
 
 # Getting BOOTNODE
 if [ $BOOTNODE_IP ]; then
@@ -82,23 +110,28 @@ if [ $BOOTNODE_IP ]; then
   BOOTNODE=$ENODE
 fi
 
-
 # Node config
 echo "Setting ENGINE_SIGNER=$ACCOUNT_ADDRESS from /home/blogpv/template-node.toml ..."
-echo "Setting NODE_NAME=$NODE_NAME in file /home/blogpv/node_config.toml ..."
+
 sed \
--e "s/{ ENGINE_SIGNER }/$ACCOUNT_ADDRESS/" \
--e "s/{ NODE_NAME }/$NODE_NAME/" \
--e "s/{ BOOTNODE }/$BOOTNODE" \
+-e "s|{ ENGINE_SIGNER }|$ACCOUNT_ADDRESS|" \
+-e "s|{ NODE_NAME }|$NODE_NAME|" \
+-e "s|{ BOOTNODE }|$BOOTNODE|" \
 /home/blogpv/template-node.toml > /home/blogpv/node_config.toml
 
+echo "Node config - /home/blogpv/node_config.toml:"
+echo "$(cat /home/blogpv/node_config.toml)"
+
 # Importing keys
-echo "Importing keystore file to chain $CHAIN_NAME"
+echo "Importing keystore file to chain $CHAIN_NAME ..."
 mkdir -p /home/blogpv/keys/$CHAIN_NAME/
 
 /home/openethereum/openethereum account import \
 -c /home/blogpv/node_config.toml \
 /home/openethereum/.local/share/io.parity.ethereum/keys/ethereum/$ACCOUNT_KEYFILE_NAME
+
+ls -lsa /home/blogpv/keys/$CHAIN_NAME/
+
 rm -f /home/openethereum/.local/share/io.parity.ethereum/keys/ethereum/$ACCOUNT_KEYFILE_NAME
 
 # echo "Imported keys: $(ls /home/blogpv/keys/$CHAIN_NAME/)"
@@ -107,15 +140,10 @@ rm -f /home/openethereum/.local/share/io.parity.ethereum/keys/ethereum/$ACCOUNT_
 # Create DB directory
 mkdir -p /home/blogpv/chains/
 
-/home/openethereum/openethereum --chain /home/blogpv/chain.json account list
+echo "Listing accoounts for chain $CHAIN_NAME ..."
+/home/openethereum/openethereum -c /home/blogpv/node_config.toml account list
 
 # Output result
-echo "Node config - /home/blogpv/node_config.toml:"
-echo "$(cat /home/blogpv/node_config.toml)"
-
-echo "Chain config - /home/blogpv/chain.json:"
-echo "$(cat /home/blogpv/chain.json)"
-
 echo "Keystore file - /home/blogpv/keys/$CHAIN_NAME/$ACCOUNT_KEYFILE_NAME:"
 echo "$(cat /home/blogpv/keys/$CHAIN_NAME/$ACCOUNT_KEYFILE_NAME)"
 
@@ -128,4 +156,5 @@ echo "ACCOUNT_KEYFILE      = /home/blogpv/keys/$CHAIN_NAME/$ACCOUNT_KEYFILE_NAME
 echo "BOOTNODE             = $BOOTNODE"
 
 # Execute the upstream command:
-exec /home/openethereum/openethereum "$@"
+echo "Params passed by --command=$@"
+exec /home/openethereum/openethereum -c /home/blogpv/node_config.toml "$@"
